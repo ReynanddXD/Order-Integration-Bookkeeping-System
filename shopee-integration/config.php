e91b150c8d01d14a1074b5fd4dbd815cbb8f0e6a
ReynandAry <165867557+ReynanddXD@users.noreply.github.com>
2025-06-19 20:15:53 +0700

    Update config.php

--- shopee-integration/config.php
+++ shopee-integration/config.php
@@ -1,7 +1,7 @@
 <?php
-define("PARTNER_ID", "2011441");
-define("PARTNER_KEY", "4a516757696b6a6f59724d6467766947494a6265486e766744475973564a4a64");
-define("REDIRECT_URL", "https://383c-2400-9800-171-7e88-c9d1-72be-343d-60f1.ngrok-free.app/rpl_project/shopee-integration/callback.php");
+define("PARTNER_ID", "");
+define("PARTNER_KEY", "");
+define("REDIRECT_URL", "/rpl_project/shopee-integration/callback.php");
 define("BASE_URL", "https://partner.shopeemobile.com");
 // define("BASE_URL", "https://partner.test-stable.shopeemobile.com");
 
